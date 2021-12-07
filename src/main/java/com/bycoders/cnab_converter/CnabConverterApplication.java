2cb7879f2084b3ed8c293b8d14a88e9c76cdaf2b
Faruque Braimo <faruquebraimo@gmail.com>
2021-12-07 10:08:27 +0200

    Language and Frameworks
    Add Java as language and Spring boot and all necessary dependencies.

---
+++ src/main/java/com/bycoders/cnab_converter/CnabConverterApplication.java
@@ -0,0 +1,13 @@
+package com.bycoders.cnab_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bycoders/cnab_converter/CnabConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycoders.cnab_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
